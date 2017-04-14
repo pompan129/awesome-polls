2a219c341e536f6770eacf30a3e18db858126cb7
Kurt Johnson <fazbat@gmail.com>
2017-04-13 19:09:18 -0700

    added Auth0 lock. works good

--- app.js
+++ app.js
@@ -6,11 +6,35 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var dotenv = require('dotenv');
+var passport = require('passport');
+var Auth0strategy = require('passport-auth0')
 
 //load env variables
 dotenv.load();
 
 var routes = require('./routes/index');
+
+var strategy = new Auth0strategy({
+  domain: process.env.AUTH0_DOMAIN,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/callback'
+},function(accessToken,refreshToken,extraParams,profile,done){
+  // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null,profile);
+});
+
+passport.use(strategy);
+
+passport.serializeUser(function(user,done){
+  done(null,user)
+});
+
+passport.deserializeUser(function(user,done){
+  done(null,user);
+})
 
 var app = express();
 
@@ -26,6 +50,9 @@ app.use(session({
   resave:true,
   saveUninitialized:true
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static(path.join(__dirname,'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,11 @@ var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 var request = require('request');
 
 
-var env = {};
+var env = {
+  AUTH0_CLIENT_ID:process.env.AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  AUTH0_CALLBACK_URL:'http://localhost:3000/callback'
+};
 
 router.get('/',function(req,res,next){
   res.render('index',{env:env});
@@ -21,7 +25,7 @@ router.get('/logout',function(req,res){
   res.redirect('/')
 });
 
-router.get('/polls',function(req,res){
+router.get('/polls',ensureLoggedIn,function(req,res){
   request('http://elections.huffingtonpost.com/pollster/api/charts.json?topic=2016-president',function(error,response,body){
     if(!error && response.statusCode == 200){
       var polls = JSON.parse(body);
@@ -36,5 +40,13 @@ router.get('/polls',function(req,res){
 router.get('/user', ensureLoggedIn,function(req,res){
   res.render('user',{env:env,user:req.user});
 });
+
+router.get('/callback',
+    passport.authenticate('auth0',{failureRedirect:'/'}),
+    function(req,res){
+      console.log(">>>router.get('/callback'");//todo
+      res.redirect(req.session.returnTo || '/polls');
+    }
+);
 
 module.exports = router;
